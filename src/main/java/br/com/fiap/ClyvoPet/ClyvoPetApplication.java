5366b6e42ff943c4d610ffaddc4bf54da638393b
enricodelesporte <enricodelesporte@hotmail.com>
2026-05-04 08:34:14 -0300

    Refact: refactoring the user and adding improvements.

--- src/main/java/br/com/fiap/ClyvoPet/ClyvoPetApplication.java
+++ src/main/java/br/com/fiap/ClyvoPet/ClyvoPetApplication.java
@@ -2,12 +2,12 @@ package br.com.fiap.ClyvoPet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ClyvoPetApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ClyvoPetApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ClyvoPetApplication.class, args);
+    }
 }
--- src/main/java/br/com/fiap/ClyvoPet/user/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.ClyvoPet.user.repository;
-
-import br.com.fiap.ClyvoPet.user.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
