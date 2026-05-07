82a21b5509d29c2c22d490d8c3b2352c4f6f0f6b
enricodelesporte <enricodelesporte@hotmail.com>
2026-05-07 08:48:08 -0300

    fix config

--- src/main/java/br/com/fiap/ClyvoPet/config/SwaggerConfig.java
+++ src/main/java/br/com/fiap/ClyvoPet/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package br.com.fiap.ClyvoPet.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,5 +20,13 @@ public class SwaggerConfig {
                         .contact(new Contact()
                                 .name("Enrico Delesporte | Felippe Modesto | Vitor Dias")
                                 .email("contato@clyvopet.com.br")));
+    }
+
+    @Bean
+    public OperationCustomizer operationCustomizer() {
+        return (operation, handlerMethod) -> {
+            operation.getParameters().removeIf(p -> p.getName().equals("pageable"));
+            return operation;
+        };
     }
 }
